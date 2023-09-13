1c5d1a05d66608282356c41b0d8b34513ff93a28
cadfrunze <marius1890@yahoo.com>
2023-09-13 21:04:24 +0300

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+Urmarire/cronmetrare calorii exercitii fizice
+
+API-uri:
+1. https://www.nutritionix.com
+2. 
---
+++ testing.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+astazi: str = datetime.now().strftime('Data: %d/%m/%Y | Ora: %H:%M')
+
