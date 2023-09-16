7d1c04b36488eaf738589e7eb1392a463149ffe4
cadfrunze <marius1890@yahoo.com>
2023-09-16 14:47:15 +0300

    update

--- main.py
+++ main.py
@@ -2,3 +2,27 @@ import requests
 
 APP_ID: str = 'af3c9ccd'
 APP_KEY: str = 'e416cbba2d1a1f6e1d02b2a513120f8e'
+URL_END_P: str = 'https://trackapi.nutritionix.com/v2/natural/exercise'
+header: dict = {
+    'x-app-id': APP_ID,
+    'x-app-key': APP_KEY,
+
+}
+
+mesaj: str = input('say: ')
+body_endpoint: dict = {
+    'query': mesaj,
+    'gender': 'male',
+    'weight_kg': '112',
+    'height_cm': '182',
+    'age': '38'
+}
+
+
+while True:
+    raspuns_exercitiu = requests.post(url=URL_END_P,
+                                      headers=header, json=body_endpoint)
+    raspuns_exercitiu.raise_for_status()
+    print(raspuns_exercitiu.json())
+    mesaj: str = input('say: ')
+    body_endpoint['query'] = mesaj
